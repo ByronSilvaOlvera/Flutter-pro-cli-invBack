7c470279911ade7dcf2dab8770b0c82cf007d7c2
Byron Silva Olvera <byronsjimb@gmail.com>
2021-04-20 12:22:40 -0500

    logueo

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const core = require('cors');
+require('dotenv').config();
+
+// DB Config
+require('./database/config').dbConnection();
+
+
+app.use(core());
+
+// App de Express
+const app = express();
+
+// Lectura y parseo del Body
+app.use( express.json() );
+
+
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+
+
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
+});
+
+
